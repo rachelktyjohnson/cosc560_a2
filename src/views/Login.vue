fb22b03df0b397902a3d1e5b8f560befce6cbe3b
Rachel Johnson <rachelktyjohnson@gmail.com>
2020-08-14 12:41:58 +1000

    :hammer: Add login validation and errors

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,20 +2,24 @@
   <main class="main-login">
     <h3>Hey, hungry bear!</h3>
     <h4>Log in so you can get some nosh</h4>
+
     <form v-on:submit.stop.prevent="checkLogin()" class="login-form">
       <div class="form-group">
         <label>Username</label>
-        <input type="text" v-model:value="username" required/>
+        <input type="text" v-model:value="username"/>
       </div>
       <div class="form-group">
         <label>Password</label>
-        <input type="password" v-model:value="password" required/>
+        <input type="password" v-model:value="password"/>
       </div>
       <div class="form-group">
         <input class="login-button" type="submit" value="Login">
       </div>
-
+      <div v-if="this.errors.length > 0" class="error">
+        <p v-for="error in errors">{{error}}</p>
+      </div>
     </form>
+
   </main>
 </template>
 <script>
@@ -28,28 +32,42 @@ export default {
   data() {
     return {
       username: "",
-      password: ""
+      password: "",
+      errors: []
     }
   },
   methods:{
     checkLogin: function(){
-      if (this.username==='admin' && this.password==='admin'){
-        //change logged in userID and go to dashboard
-        this.$store.commit('changeUser',0);
-        this.$router.push('orders');
-      } else if (this.username==='adam' && this.password==='adam'){
-        //change logged in userID and go to landing
-        this.$store.commit('changeUser',1);
-        this.$router.push('listing');
-      } else if (this.username==='ben' && this.password==='ben'){
-        //change logged in userID and go to landing
-        this.$store.commit('changeUser',2);
-        this.$router.push('listing');
-      }  else if (this.username==='charlie' && this.password==='charlie'){
-        //change logged in userID and go to landing
-        this.$store.commit('changeUser',3);
-        this.$router.push('listing');
+      this.errors = [];
+      if (!this.username || !this.password){
+        if (!this.username){
+          this.errors.push("Please enter a username");
+        }
+        if (!this.password){
+          this.errors.push("Please enter a password");
+        }
+      } else {
+        if (this.username==='admin' && this.password==='admin'){
+          //change logged in userID and go to dashboard
+          this.$store.commit('changeUser',0);
+          this.$router.push('orders');
+        } else if (this.username==='adam' && this.password==='adam'){
+          //change logged in userID and go to landing
+          this.$store.commit('changeUser',1);
+          this.$router.push('listing');
+        } else if (this.username==='ben' && this.password==='ben'){
+          //change logged in userID and go to landing
+          this.$store.commit('changeUser',2);
+          this.$router.push('listing');
+        }  else if (this.username==='charlie' && this.password==='charlie'){
+          //change logged in userID and go to landing
+          this.$store.commit('changeUser',3);
+          this.$router.push('listing');
+        } else {
+          this.errors.push("Oops! Looks like you don't have an account with us.")
+        }
       }
+
     }
   }
 }
