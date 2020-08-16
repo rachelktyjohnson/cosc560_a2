fef5bc47da7674cde56861d43cc2cdfd593b8e00
Rachel Johnson <rachelktyjohnson@gmail.com>
2020-08-16 15:36:09 +1000

    :hammer: Pie chart working

--- src/components/LineChartComponent.vue
+++ src/components/LineChartComponent.vue
@@ -1,43 +1,43 @@
 <template>
   <div>
-    <p>{{ message }}</p>
-    <svg :height="height" :width="width"></svg>
-    <p>{{date}}</p>
-    <p>{{data}}</p>
+    <svg id="line_chart"></svg>
   </div>
 
 </template>
 <script>
 import * as d3 from 'd3';
 
-export default{
+export default {
   name: "LineChartComponent",
   data() {
     return {
       message: "Hello from the Chart Component!",
-      margin: {top:20, right:30, bottom:50, left:40},
       width: 500,
       height: 300,
-      dataPoints: 30,
+      dataPoints: 7,
       data: [],
-      date: new Date(2019,0,1),
-      sales: 50.21
+      date: new Date(2019, 0, 1),
+      sales: 50.21,
+      margin: { top: 20, right: 30, bottom: 50, left: 40 }
     }
   },
-  created: function(){
+  created: function () {
     //populate Data with the first X dataPoints
-    for (let i=0; i<=this.dataPoints; i++){
+    for (let i = 0; i <= this.dataPoints; i++) {
       this.data = this.generateData();
     }
   },
-  mounted: function(){
-    // window.setInterval(()=>{
-    //   this.generateData()
-    //   this.data.shift();
-    // },1000)
+  mounted: function () {
+
+    //this.renderChart(this.data);
+     // window.setInterval(()=>{
+     //   this.generateData();
+     //   this.renderChart(this.data);
+     //   this.data.shift();
+     // },1000)
   },
-  methods:{
-    generateData: function(){
+  methods: {
+    generateData: function () {
       let MAX = 1000;
       let MIN = 10;
 
@@ -46,12 +46,82 @@ export default{
 
       //set the next date
       let nextDay = new Date(this.date);
-      nextDay.setDate(nextDay.getDate()+1);
+      nextDay.setDate(nextDay.getDate() + 1);
       this.date = nextDay;
 
       //set the next sales (2 decimal places)
       this.sales = parseFloat((Math.random() * (MAX - MIN) + MIN).toFixed(2));
       return this.data;
+    },
+    renderChart(data) {
+      //group element for the entire chart
+      let svg_group = d3.select("#line_chart").append("g");
+
+      // y axis
+      let y_scale = d3.scaleLinear()
+          .domain([0, d3.max(this.data, d => d.sales)]).nice()
+          .range([this.height - this.margin.bottom, this.margin.top]);
+      let y_axis = svg_group.append("g")
+          .attr("transform", `translate(${this.margin.left},0)`)
+          .attr('class', 'y axis')
+          .call(d3.axisLeft(y_scale))
+          .call(g => g.select(".domain"))
+          .call(g => g.select(".tick:last-of-type text").clone()
+              .attr("x", 3)
+              .attr("text-anchor", "start")
+              .attr("font-weight", "bold")
+              .text(this.data.y));
+
+
+      // define a group for the x-axis and the labels
+      let x_axis = svg_group.append("g")
+          .attr('class', 'x axis');
+
+      // ex3.3 Step 8: line generator
+      let line = d3.line()
+          .defined(d => !isNaN(d.sales))
+          .x(d => x_scale(d.date))
+          .y(d => y_scale(d.sales));
+
+      // ex3.3 Step 9: the actual line in the svg
+      let the_line = svg_group
+          .append("path")
+          .attr("class", "line");
+      // x axis
+      // let x_scale;
+      // define a group for the x-axis and the labels
+      x_axis = svg_group.append("g")
+          .attr('class', 'x axis');
+
+      console.log(x_axis);
+
+      // ex3.3 Step 8: line generator
+      line = d3.line()
+          .defined(d => !isNaN(d.sales))
+          .x(d => x_scale(d.date))
+          .y(d => y_scale(d.sales));
+
+      // ex3.3 Step 9: the actual line in the svg
+      the_line = svg_group
+          .append("path")
+          .attr("class", "line");
+
+      // (re)calculate the x-scale and x-axis for the new data
+      let x_scale = d3.scaleUtc()
+          .domain(d3.extent(data, d => d.date))
+          .range([this.margin.left, this.width - this.margin.right]);
+      x_axis.attr('class', 'x axis')
+          .attr("transform", `translate(0,${this.height - this.margin.bottom})`)
+          .call(d3.axisBottom(x_scale).ticks(this.width / 100).tickSizeOuter(0));
+      // draw the line with data
+      the_line.datum(data)
+          .attr("fill", "none")
+          .attr("stroke", "steelblue")
+          .attr("stroke-width", 1.5)
+          .attr("stroke-linejoin", "round")
+          .attr("stroke-linecap", "round")
+          .attr("d", line);
+
     }
   }
 }
--- src/components/PieChartComponent.vue
+++ src/components/PieChartComponent.vue
@@ -1,55 +1,109 @@
 <template>
   <div>
-    <p>{{ message }}</p>
-    <svg :height="height" :width="width"></svg>
+    <div id="pie_dataviz"></div>
+
     <p>{{date}}</p>
-    <p>{{data}}</p>
   </div>
 
 </template>
 <script>
 import * as d3 from 'd3';
-
 export default{
   name: "PieChartComponent",
   data() {
     return {
       message: "Hello from the Pie Component!",
-      margin: {top:20, right:30, bottom:50, left:40},
-      width: 500,
-      height: 500,
-      restaurants: 10,
-      data: [
-        {restaurant:"Big B's", sales:364.34},
-        {restaurant:"Rene's Pizza Place", sales:753.12},
-        {restaurant:"Golden Ma Lun", sales:424.74},
-        {restaurant:"Main Street Kebabs", sales:119.03},
-        {restaurant:"The Grounds Keeper Cafe", sales:296.23},
-        {restaurant:"Westpoint Noodle and Dumpling Bar", sales:585.94},
-      ],
-      date: new Date(2019,0,31)
+      data: {
+        "a": 364.34,
+        "b": 753.12,
+        "c": 424.74,
+        "d": 119.03,
+        "e": 296.23,
+        "f": 585.94
+      },
+      date: new Date(2019,0,1)
     }
   },
   mounted: function(){
-     // window.setInterval(()=>{
-     //   this.generateData();
-     // },1000)
+      // set the dimensions and margins of the graph
+      let width = 450
+      let height = 450
+      let margin = 40
+
+      // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+      let radius = Math.min(width, height) / 2 - margin
+
+
+
+      this.chart(width,height,radius)
+
+      window.setInterval(()=>{
+        this.generateData();
+        //clear the g transform
+        this.chart(width,height,radius);
+      },2000)
   },
   methods:{
+    chart:function(width,height,radius){
+      //clear g transform
+      d3.select("#pie_dataviz svg").remove();
+      console.log("cleared");
+      // append the svg object to the div called 'my_dataviz'
+      let svg = d3.select("#pie_dataviz")
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      // Compute the position of each group on the pie:
+      let pie = d3.pie()
+          .value(function(d) {return d.value; })
+      let data_ready = pie(d3.entries(this.data))
+      // Now I know that group A goes from 0 degrees to x degrees and so on.
+
+      // shape helper to build arcs:
+      let arcGenerator = d3.arc()
+          .innerRadius(0)
+          .outerRadius(radius)
+
+      // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+      svg
+          .selectAll('mySlices')
+          .data(data_ready)
+          .enter()
+          .append('path')
+          .attr('d', arcGenerator)
+          .attr('fill', "black")
+          .attr("stroke", "black")
+          .style("stroke-width", "2px")
+          .style("opacity", 0.4)
+
+      // Now add the annotation. Use the centroid method to get the best coordinates
+      svg
+          .selectAll('mySlices')
+          .data(data_ready)
+          .enter()
+          .append('text')
+          .text(function(d){ return d.data.key+" $"+ d.data.value})
+          .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+          .style("text-anchor", "middle")
+          .style("font-size", 10)
+
+    },
     generateData: function(){
       let MAX = 1000;
-      let MIN = 10;
+      let MIN = 100;
 
       //set the next date
       let nextDay = new Date(this.date);
       nextDay.setDate(nextDay.getDate()+1);
       this.date = nextDay;
 
-
-      //set the next sales (2 decimal places)
-      for (let i=0; i<this.data.length; i++){
-        this.data[i].sales = parseFloat((Math.random() * (MAX - MIN) + MIN).toFixed(2));
+      for (let key in this.data){
+        this.data[key] = parseFloat((Math.random() * (MAX - MIN) + MIN).toFixed(2));
       }
+      console.log(this.data);
       return this.data;
     }
   }
