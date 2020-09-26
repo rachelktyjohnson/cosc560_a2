025805a5333e1e39a3990d0764a083d96d15cf5b
Rachel Johnson <rachelktyjohnson@gmail.com>
2020-09-26 16:44:50 +1000

    Hook up notifications

--- src/components/NotificationsComponent.vue
+++ src/components/NotificationsComponent.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="notifications">
   <div v-for="(notification) in notifications.slice(0,5)" class="notification">
-    <h6>{{ notification.contents }}</h6>
+    <h6>{{ notification.content }}</h6>
     <p>{{ formatDateTime(notification.datetime) }}</p>
   </div>
   <p v-if="notifications.length > 5">(Only the latest 5 notifications will be shown)</p>
@@ -10,17 +10,24 @@
 </template>
 <script>
 import { dateMixin } from '../mixins/dateMixin';
+import axios from 'axios';
+
 export default {
   name: 'NotificationsComponent',
   mixins: [dateMixin],
   data() {
-    return {}
-  },
-  computed: {
-    notifications() {
-      let currentUser = this.$store.getters.getCurrentUser;
-      return currentUser.notifications
+    return {
+      notifications: []
     }
+  },
+  created() {
+    axios.get('http://localhost:9000/notifications/byuser/'+this.$store.state.loggedIn.userID)
+        .then (response => {
+          this.notifications = response.data.data
+        })
+        .catch (err =>{
+          this.errors.push(err)
+        })
   }
 }
 </script>
